174e03aa8a0171311c2cc2d88b6ef6e87200da25
Luke Dyson <luke.dyson@flyinganvil.co.uk>
2014-05-23 15:38:46 +0100

    Adding cookie class

---
+++ src/Cookie.js
@@ -0,0 +1,72 @@
+// todo: requires undersocre :)
+jsutils.Cookie = (function () {
+    'use strict';
+    var cookies = {},
+        findAll = function() {
+            _(document.cookie.split(';'))
+                .chain()
+                .map(function(m) {
+                    return m.replace(/^\s+/, '').replace(/\s+$/, '');
+                })
+                .each(function(c) {
+                    var arr = c.split('='),
+                        key = arr[0],
+                        value = null;
+                    var size = _.size(arr);
+                    if (size > 1) {
+                        value = arr.slice(1).join('');
+                    }
+                    cookies[key] = value;
+                });
+            return cookies;
+        },
+        defaultLifetime = (1000 * 60 * 60 * 24 * 365 * 10), // 10 years
+        today = new Date();
+
+    // retrieving all existing cookies
+    findAll();
+
+    return {
+        all: function() {
+            return cookies;
+        },
+        find: function(name) {
+            return _.isUndefined(cookies[name]) ? null : cookies[name];
+        },
+        has: function(name) {
+            return _.isUndefined(cookies[name]) ? false : true;
+        },
+        create: function(name, value, time) {
+            var offset = (_.isUndefined(time)) ? defaultLifetime : (time * 1000),
+                expiresAt = new Date(today.getTime() + offset);
+
+            var cookie = _.map({
+                name: escape(value),
+                expires: expiresAt.toGMTString(),
+                path: '/'
+            }, function(value, key) {
+                return [(key === 'name') ? name : key, value].join('=');
+            }).join(';');
+
+            document.cookie = cookie;
+            findAll();
+            return cookies[name];
+        },
+        remove: function(name) {
+            var self = this;
+            self.create(name, null, -1000000);
+            delete cookies[name];
+        },
+        // for tests only
+        init: function() {
+            findAll();
+        },
+        clear: function() {
+            var self = this;
+            _(cookies).each(function(value, name){
+                self.remove(name);
+            });
+            cookies = {};
+        }
+    };
+}());
---
+++ test/CookieSpec.js
@@ -0,0 +1,64 @@
+describe('Cookie Spec', function () {
+    beforeEach(function() {
+        document.cookie = 'cookie=test';
+        document.cookie = 'jsutils=awesomeness';
+        jsutils.Cookie.init();
+    });
+    afterEach(function() {
+        jsutils.Cookie.clear();
+    });
+
+    it('Can read in all of the currently created cookies', function () {
+        var mockCookies = {
+            cookie: 'test',
+            jsutils: 'awesomeness'
+        };
+
+        expect(_.isEqual(jsutils.Cookie.all(), mockCookies));
+    });
+
+    it('Can clear all of the cookies', function () {
+        var mockCookies = {
+            cookie: 'test',
+            jsutils: 'awesomeness'
+        };
+
+        expect(_.isEqual(jsutils.Cookie.all(), mockCookies)).to.equal(true);
+
+        jsutils.Cookie.clear();
+
+        expect(_.isEqual(jsutils.Cookie.all(), {})).to.equal(true);
+    });
+
+    it('Can find a cookie by its name', function() {
+        expect(jsutils.Cookie.find('cookie')).to.equal('test');
+        expect(jsutils.Cookie.find('jsutils')).to.equal('awesomeness');
+        expect(jsutils.Cookie.find('undefined')).to.equal(null);
+    });
+
+    it('Can test if a cookie exists', function() {
+        expect(jsutils.Cookie.has('cookie')).to.equal(true);
+        expect(jsutils.Cookie.has('jsutils')).to.equal(true);
+        expect(jsutils.Cookie.has('undefined')).to.equal(false);
+    });
+
+    it('Can create a new cookie', function() {
+        expect(jsutils.Cookie.find('newCookie')).to.equal(null);
+        expect(jsutils.Cookie.has('newCookie')).to.equal(false);
+
+        jsutils.Cookie.create('newCookie', 'huzzah');
+
+        expect(jsutils.Cookie.find('newCookie')).to.equal('huzzah');
+        expect(jsutils.Cookie.has('newCookie')).to.equal(true);
+    });
+
+    it('Can remove a cookie', function() {
+        expect(jsutils.Cookie.find('cookie')).to.equal('test');
+        expect(jsutils.Cookie.has('cookie')).to.equal(true);
+
+        jsutils.Cookie.remove('cookie');
+
+        expect(jsutils.Cookie.find('cookie')).to.equal(null);
+        expect(jsutils.Cookie.has('cookie')).to.equal(false);
+    });
+});
